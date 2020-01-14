c071847706e38e68faad03339bb079a8fe4836d0
hyogeon <men1219@hanmail.net>
2020-01-15 04:05:22 +0900

    feat: HelloController

---
+++ src/main/java/com/hgkim/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,14 @@
+package com.hgkim.book.springboot.web.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
