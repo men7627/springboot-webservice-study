1767c9cd91a9733555c971650298fafd62ff8a6f
hyogeon <men1219@hanmail.net>
2020-01-18 00:04:09 +0900

    feat: Posts update, findById

--- src/main/java/com/hgkim/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/hgkim/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.hgkim.book.springboot.service.posts;
 
+import com.hgkim.book.springboot.domain.posts.Posts;
 import com.hgkim.book.springboot.domain.posts.PostsRepository;
+import com.hgkim.book.springboot.web.dto.PostsResponseDto;
 import com.hgkim.book.springboot.web.dto.PostsSaveRequestDto;
+import com.hgkim.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -18,5 +21,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new NotFoundAuthorException(id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new NotFoundAuthorException(id));
+
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/com/hgkim/book/springboot/web/PostsApiController.java
+++ src/main/java/com/hgkim/book/springboot/web/PostsApiController.java
@@ -1,10 +1,10 @@
 package com.hgkim.book.springboot.web;
 
 import com.hgkim.book.springboot.service.posts.PostsService;
+import com.hgkim.book.springboot.web.dto.PostsResponseDto;
 import com.hgkim.book.springboot.web.dto.PostsSaveRequestDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.hgkim.book.springboot.web.dto.PostsUpdateRequestDto;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PostsApiController {
@@ -18,6 +18,16 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 
 }
