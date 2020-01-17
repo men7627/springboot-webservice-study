a6a9e0ae2b15953d9bdbce0922231c357f871c8b
hyogeon <men1219@hanmail.net>
2020-01-18 01:24:11 +0900

    feat: BaseTimeEntity

--- src/main/java/com/hgkim/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/hgkim/book/springboot/domain/posts/Posts.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,7 +18,7 @@ public class Posts {
     @Column(length = 500, nullable = false)
     private String title;
 
-    @Column(columnDefinition = "TEXT", nullable =  false)
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
     private String author;
