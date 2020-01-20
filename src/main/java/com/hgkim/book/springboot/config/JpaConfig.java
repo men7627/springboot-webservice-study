3f3c2beaf3eccc185f9a4cfc7d82162bb759c82b
hyogeon <men1219@hanmail.net>
2020-01-21 05:27:37 +0900

    fix: devide @SpringBootApplication and @EnableJpaAuditing
     - feat: JpaConfig

---
+++ src/main/java/com/hgkim/book/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.hgkim.book.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
