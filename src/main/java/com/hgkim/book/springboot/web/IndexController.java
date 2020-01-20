898c90efe85c5e8359b04abf583df697e55205d6
hyogeon <men1219@hanmail.net>
2020-01-21 01:29:48 +0900

    feat: Google Login

--- src/main/java/com/hgkim/book/springboot/web/IndexController.java
+++ src/main/java/com/hgkim/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hgkim.book.springboot.web;
 
+import com.hgkim.book.springboot.config.auth.dto.SessionUser;
 import com.hgkim.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -7,15 +8,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
+
+        if (sessionUser != null) {
+            model.addAttribute("userName", sessionUser.getName());
+        }
         return "index";
     }
 
