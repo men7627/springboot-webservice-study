23b14e9b1926548ca57c317b6e66d5d5b66b55f3
hyogeon <men1219@hanmail.net>
2020-01-21 04:41:44 +0900

    feat: naver login

--- src/main/java/com/hgkim/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/hgkim/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -25,25 +25,40 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                            .name((String) response.get("name"))
+                            .email((String) response.get("email"))
+                            .picture((String) response.get("picture"))
+                            .nameAttributeKey(userNameAttributeName)
+                            .attributes(response)
+                            .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
-                            .name((String)attributes.get("name"))
-                            .email((String)attributes.get("email"))
-                            .picture((String)attributes.get("picture"))
-                            .attributes(attributes)
-                            .nameAttributeKey(userNameAttributeName)
-                            .build();
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     public User toEntity() {
         return User.builder()
-                    .name(name)
-                    .email(email)
-                    .picture(picture)
-                    .role(Role.GUEST)
-                    .build();
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
     }
 }
