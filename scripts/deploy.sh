c2a655db4f750ad0999362a14dda6a471557838b
hyogeon <men1219@hanmail.net>
2020-02-09 21:49:49 +0900

    fix: deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ PROJECT_NAME=springboot-webservice-study
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "현재 구동 중인 애플리케이션 pid 확인"
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl springboot-webservice-study | grep jar | awk '{print $1}')
 
@@ -30,4 +30,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>$1 $
+nohup java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
